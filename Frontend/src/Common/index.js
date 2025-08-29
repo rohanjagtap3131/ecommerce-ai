2f7a8767eaa32ddf7d29a054333c3ef426ce5c71
Rohanjagtap3131 <rohanjagtap78389@gmail.com>
2025-08-29 14:39:04 +0530

    Your descriptive commit message

--- Frontend/src/Common/index.js
+++ Frontend/src/Common/index.js
@@ -2,112 +2,112 @@
 const backendDomain = import.meta.env.VITE_BACKEND_URL;
 const summuryAPI = {
     Signup: {
-        url: `${backendDomain}/signup`,
+        url: `${backendDomain}/api/signup`,
         method: "post"
     },
     Signin: {
-        url: `${backendDomain}/signin`,
+        url: `${backendDomain}/api/signin`,
         method: "post"
     },
 
     UserDetails: {
-        url: `${backendDomain}/user-details`,
+        url: `${backendDomain}/api/user-details`,
         method: "get"
     },
     userLogout: {
-        url: `${backendDomain}/user-logout`,
+        url: `${backendDomain}/api/user-logout`,
         method: "get"
     },
     allUsers: {
-        url: `${backendDomain}/all-Users`,
+        url: `${backendDomain}/api/all-Users`,
         method: "get"
     },
     updateUser: (id) => ({
-        url: `${backendDomain}/update-user/${id}`,
+        url: `${backendDomain}/api/update-user/${id}`,
         method: "put"
     }),
     uploadProduct: {
-        url: `${backendDomain}/upload-product`,
+        url: `${backendDomain}/api/upload-product`,
         method: "post"
     },
     getProduct: {
-        url: `${backendDomain}/get-Product`,
+        url: `${backendDomain}/api/get-Product`,
         method: "get"
     },
     updateProduct: (id) => ({
-        url: `${backendDomain}/update-product/${id}`,
+        url: `${backendDomain}/api/update-product/${id}`,
         method: "put"
     }),
     categoryProduct: {
-        url: `${backendDomain}/get-categeoryProduct`,
+        url: `${backendDomain}/api/get-categeoryProduct`,
         method: "get"
     },
     categoryProdutes: {
-        url: `${backendDomain}/categeory-product`,
+        url: `${backendDomain}/api/categeory-product`,
         method: "post"
     },
     productDetails: {
-        url: `${backendDomain}/product-details`,
+        url: `${backendDomain}/api/product-details`,
         method: "post"
     },
     addToCart: {
-        url: `${backendDomain}/addtocart`,
+        url: `${backendDomain}/api/addtocart`,
         method: "post"
     },
     CartProductCount: {
-        url: `${backendDomain}/countCartProduct`,
+        url: `${backendDomain}/api/countCartProduct`,
         method: "get"
     },
     viewCart: {
-        url: `${backendDomain}/view-cartProduct`,
+        url: `${backendDomain}/api/view-cartProduct`,
         method: "get"
     },
     viewCartUpdate: (id) => ({
-        url: `${backendDomain}/update-cartProduct/${id}`,
+        url: `${backendDomain}/api/update-cartProduct/${id}`,
         method: "put"
     }),
     viewCartDelete: (id) => ({
-        url: `${backendDomain}/remove-cartProduct/${id}`,
+        url: `${backendDomain}/api/remove-cartProduct/${id}`,
         method: "delete",
     }),
     searchProduct: {
-        url: `${backendDomain}/search`,
+        url: `${backendDomain}/api/search`,
         method: "get"
     },
     filter: {
-        url: `${backendDomain}/filter-product`,
+        url: `${backendDomain}/api/filter-product`,
         method: "post"
     },
     createOrder: {
-        url: `${backendDomain}/create-order`,
+        url: `${backendDomain}/api/create-order`,
         method: "post",
     },
     verifyPayment: {
-        url: `${backendDomain}/verify-payment`,
+        url: `${backendDomain}/api/verify-payment`,
         method: "post",
     },
     getOrders: {
-        url: `${backendDomain}/my-orders`,
+        url: `${backendDomain}/api/my-orders`,
         method: "get",
     },
     getAllOrders: {
-        url: `${backendDomain}/all-orders`,
+        url: `${backendDomain}/api/all-orders`,
         method: "get",
     },
     updateOrders: (id) => ({
-        url: `${backendDomain}/update-tracking/${id}`,
+        url: `${backendDomain}/api/update-tracking/${id}`,
         method: "put",
     }),
     cancelOrders: (id) => ({
-        url: `${backendDomain}/cancel/${id}`,
+        url: `${backendDomain}/api/cancel/${id}`,
         method: "put",
     }),
     deleteOrders: (id) => ({
-        url: `${backendDomain}/order/${id}`,
+        url: `${backendDomain}/api/order/${id}`,
         method: "delete",
     }),
     aiChat: {
-        url: `${backendDomain}/chat`,
+        url: `${backendDomain}/api/chat`,
         method: "post",
     }
 
